44de25eed276432e2a378a424a7944decf43b532
Viviana Lugo <vivianacruz291@gmail.com>
2025-05-26 17:50:06 -0600

    quedó la página de la base de datos de clasificaciones en ek navegador web

--- ProyectoFinalSem/Repositorio/IRepositorioClasificaciones.cs
+++ ProyectoFinalSem/Repositorio/IRepositorioClasificaciones.cs
@@ -4,7 +4,22 @@ namespace ProyectoFinalSem.Repositorio
 {
     public interface IRepositorioClasificaciones
     {
-        //leer las clasificaciones
+        // Obtener todas las clasificaciones
+        Task<List<Clasificacion>> GetAll();
+
+        // Obtener una clasificación por ID
+        Task<Clasificacion?> Get(int id);
+
+        // Agregar una nueva clasificación
+        Task<Clasificacion?> Add(Clasificacion clasificacion);
+
+        // Actualizar una clasificación existente
+        Task Update(int id, Clasificacion clasificacion);
+
+        // Eliminar una clasificación
+        Task Delete(int id);
+
+        // Método que ya tenías (puedes usarlo como alias de GetAll si quieres)
         Task<List<Clasificacion>> GetClasificaciones();
     }
 }
--- ProyectoFinalSem/Repositorio/RepositorioClasificaciones.cs
+++ ProyectoFinalSem/Repositorio/RepositorioClasificaciones.cs
@@ -11,9 +11,53 @@ namespace ProyectoFinalSem.Repositorio
         {
             _context = context;
         }
-        public async Task<List<Clasificacion>> GetClasificaciones()
+
+        // Obtener todas las clasificaciones
+        public async Task<List<Clasificacion>> GetAll()
         {
             return await _context.Clasificaciones.ToListAsync();
+        }
+
+        // Obtener una clasificación por ID
+        public async Task<Clasificacion?> Get(int id)
+        {
+            return await _context.Clasificaciones.FindAsync(id);
+        }
+
+        // Agregar una nueva clasificación
+        public async Task<Clasificacion?> Add(Clasificacion clasificacion)
+        {
+            await _context.Clasificaciones.AddAsync(clasificacion);
+            await _context.SaveChangesAsync();
+            return clasificacion;
+        }
+
+        // Actualizar una clasificación existente
+        public async Task Update(int id, Clasificacion clasificacion)
+        {
+            var existente = await _context.Clasificaciones.FindAsync(id);
+            if (existente != null)
+            {
+                existente.Nombre = clasificacion.Nombre;
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        // Eliminar una clasificación
+        public async Task Delete(int id)
+        {
+            var clasificacion = await _context.Clasificaciones.FindAsync(id);
+            if (clasificacion != null)
+            {
+                _context.Clasificaciones.Remove(clasificacion);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        // Método adicional si necesitas compatibilidad con GetClasificaciones()
+        public async Task<List<Clasificacion>> GetClasificaciones()
+        {
+            return await GetAll();
         }
     }
 }
